ee6acef382f466e554be88d0aedf4809f7ccb624
Mike Hickman <mike@pdxmike.com>
2020-04-30 07:41:38 -0700

    Condense code

--- src/api/index.js
+++ src/api/index.js
@@ -6,12 +6,7 @@ export const fetchData = async () => {
   try {
     const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)  // destructuring data out of the full response because it has my meat and potatoes
 
-    const modifiedData = {
-      confirmed:  confirmed,
-      recovered: recovered,
-      deaths: deaths,
-      lastUpdate: lastUpdate,
-    }
+    const modifiedData = { confirmed, recovered, deaths, lastUpdate }
 
     return modifiedData;
   } catch (error) {
