c00f57808955e02953a80f3595607e269fbae93b
Mike Hickman <mike@pdxmike.com>
2020-04-30 07:38:58 -0700

    Further destructure from data

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,13 @@ const url = 'https://covid19.mathdro.id/api'
 
 export const fetchData = async () => {
   try {
-    const { data } = await axios.get(url)  // destructuring data out of the full response because it has my meat and potatoes
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)  // destructuring data out of the full response because it has my meat and potatoes
 
     const modifiedData = {
-      confirmed:  data.confirmed,
-      recovered: data.recovered,
-      deaths: data.deaths,
-      lastUpdate: data.lastUpdate,
+      confirmed:  confirmed,
+      recovered: recovered,
+      deaths: deaths,
+      lastUpdate: lastUpdate,
     }
 
     return modifiedData;
