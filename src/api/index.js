be8776ab1d5945546095294512063f317edf5490
Mike Hickman <mike@pdxmike.com>
2020-04-30 07:42:51 -0700

    Condense even more

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,8 @@ const url = 'https://covid19.mathdro.id/api'
 
 export const fetchData = async () => {
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)  // destructuring data out of the full response because it has my meat and potatoes
-
-    const modifiedData = { confirmed, recovered, deaths, lastUpdate }
-
-    return modifiedData;
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url)
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
 
   }
