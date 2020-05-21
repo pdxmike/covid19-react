d1b194f6e0cd4d00a8a77f29ecea0291b853c002
Mike Hickman <mike@pdxmike.com>
2020-05-21 07:29:51 -0700

    Add handleCountryChange to initially obtain data from selected country

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import { fetchData } from './api';
 class App extends React.Component {
   state = {
     data: {},
+    country: '',
   }
 
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
   }
 
   render() {
@@ -20,7 +25,7 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart />
       </div>
     )
