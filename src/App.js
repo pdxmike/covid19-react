5eded5bd4913e6dc145d4da38d811382646f8152
Mike Hickman <mike@pdxmike.com>
2020-05-21 08:19:27 -0700

    Add logo image and update styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from './api';
+import logo from './images/logo.png'
 
 class App extends React.Component {
   state = {
@@ -25,6 +26,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
+        <img src={logo} className={styles.logo} alt='COVID-19 Logo' />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
