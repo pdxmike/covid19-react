02c34f360a73ea8aa0b1caaf35df01531b8c8bea
Mike Hickman <mike@pdxmike.com>
2020-05-21 06:34:47 -0700

    Add chart.js and pull initial data

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ class App extends React.Component {
     return (
       <div>
         <Cards data={data} />
-        <Chart />
         <CountryPicker />
+        <Chart />
       </div>
     )
   }
